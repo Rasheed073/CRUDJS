91fa144f01a227e6486c749a5afa6bac5f43c1fb
Rasheed073 <rasheedphrasheed@gmail.com>
2023-03-08 21:59:40 +0530

    updated CRUD

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
- var selectedRow = null;
- 
- function onFormSubmit(){
-    
-event.preventDefault()
-var formData = readFormData()
+var selectedRow = null;
 
-if (selectedRow === null){
-    insertNewRecord(formData)
+function onFormSubmit() {
+    var formData = readFormData()
+    if (selectedRow == null) {
+        insertNewRecord(formData)
+    }
+    else {
+        updateRecord(formData)
+    }
+    resetForm()
 }
-else {
-    updateRecord(formData)
-}
-resetForm()
- }
 
- // retrive the data 
 
- function readFormData(){
+//  retrave the data 
+
+function readFormData(){
     var formData = {};
     formData["productCode"] = document.getElementById("productCode").value;
     formData["product"] = document.getElementById("product").value;
     formData["qty"] = document.getElementById("qty").value;
     formData["perPrice"] = document.getElementById("perPrice").value;
+   
     return formData;
-  
  } 
+
 
  //insert the data
 
  function insertNewRecord(data){
-    var table = document.getElementById('storeList').getElementsByTagName('tbody')[0]
+    var table = document.getElementById('storeList').getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length)
     var cell1 = newRow.insertCell(0);
         cell1.innerHTML = data.productCode
@@ -48,10 +47,10 @@ resetForm()
 
  function onEdit(td){
     selectedRow = td.parentElement.parentElement
-    document.getElementById('productCode').value = selectedRow.cell[0].innerHTML;
-    document.getElementById('product').value = selectedRow.cell[1].innerHTML;
-    document.getElementById('qty').value = selectedRow.cell[2].innerHTML;
-    document.getElementById('perPrice').value = selectedRow.cell[3].innerHTML;
+    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
 
  }
 
@@ -66,17 +65,18 @@ resetForm()
 // delete the data
 
 function onDelete(td){
-    if(confirm('Do you want to delete')){
+    if(confirm('Do you want to delete?'))
         row = td.parentElement.parentElement;
         document.getElementById('storeList').deleteRow(row.rowIndex)
-    }resetForm()
+        resetForm()
 }
 
 //reset data (update)
 
 function resetForm(){
-    document.getElementById('productCode').value = '';
-    document.getElementById('product').value = '';
-    document.getElementById('qty').value = '';
-    document.getElementById('perPrice').value = '';
+    document.getElementById('productCode').value = "";
+    document.getElementById('product').value = "";
+    document.getElementById('qty').value = "";
+    document.getElementById('perPrice').value = "";
+    selectedRow = null
 }
